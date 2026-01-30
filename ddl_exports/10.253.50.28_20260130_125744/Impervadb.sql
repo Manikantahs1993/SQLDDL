02b64aebc2230422ce6ba5732b9be99ff98bc10c
svc-pamrdpaccess <svc-pamrdpaccess@ANGELTRADE.COM>
2026-01-30 15:31:15 +0530

    DDL Backup - 30-01-2026 15:31:15.77

--- ddl_exports/10.253.50.28_20260130_125744/Impervadb.sql
+++
@@ -1,8 +0,0 @@
--- DDL Export
--- Server: 10.253.50.28
--- Database: Impervadb
--- Exported: 2026-01-30T12:57:49.783569
-
-USE Impervadb;
-GO
-
---
+++ ddl_exports/10.253.50.28_20260130_153027/Impervadb.sql
@@ -0,0 +1,8 @@
+-- DDL Export
+-- Server: 10.253.50.28
+-- Database: Impervadb
+-- Exported: 2026-01-30T15:30:39.899083
+
+USE Impervadb;
+GO
+
